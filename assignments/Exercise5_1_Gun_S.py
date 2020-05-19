1d6f0338f904543f3cb32cb06722162265665483
K.kazma <65392439+Kings-Ggs-Arthers@users.noreply.github.com>
2020-05-19 14:01:48 +0700

    Add files via upload

---
+++ assignments/Exercise5_1_Gun_S.py
@@ -0,0 +1,7 @@
+print(" ----- Calculating -----")
+Num1 = int(input("Number 1 : "))
+Num2 = int(input("Number 2 : "))
+print(Num1, "+", Num2,"=",Num1 + Num2)
+print(Num1, "-", Num2,"=",Num1 - Num2)
+print(Num1, "*", Num2,"=",Num1 * Num2)
+print(Num1, "/", Num2,"=",Num1 / Num2)
