238bf2d83df01eaef899a3eca0b73004ecd5a902
K.kazma <65392439+Kings-Ggs-Arthers@users.noreply.github.com>
2020-05-25 12:50:39 +0700

    Add files via upload

---
+++ assignments/Lecture50_Gun_S.py
@@ -0,0 +1,14 @@
+def addNum(x,y):
+    print(x + y)
+def subNum(x,y):
+    print(x - y)
+def multiNum (x,y):
+    print(x * y)
+def divideNum (x,y):
+    print(x / y)
+x = int(input("x :"))
+y = int(input("y :"))
+addNum(x,y)
+subNum(x,y)
+multiNum(x,y)
+divideNum(x,y)
