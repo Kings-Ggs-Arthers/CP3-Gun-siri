4ca9811e33a9ee0fd4a36a9fd02f0c5bdab83dbd
K.kazma <65392439+Kings-Ggs-Arthers@users.noreply.github.com>
2020-05-30 16:44:08 +0700

    Add files via upload

---
+++ assignments/Lecture71_Gun_S.py
@@ -0,0 +1,21 @@
+menuList = []
+priceList = []
+
+def showBill():
+    print(" ***** Food Center ***** ")
+    totalPrice = 0
+    for number in range(len(menuList)):
+        print(menuList[number], priceList[number])
+        totalPrice += int(priceList[number])
+    print("Total :", totalPrice)
+
+while True:
+    menuName = input("Please Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+
+showBill()
